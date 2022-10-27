ac6eba5c045d8a22529b9153097d36397890f3d8
doff <zend__7@163.com>
2022-10-27 17:37:38 +0800

    init

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -11,11 +11,8 @@ declare(strict_types=1);
  */
 namespace App\Controller;
 
-use Hyperf\HttpServer\Annotation\PostMapping;
-
 class IndexController extends AbstractController
 {
-    #[PostMapping(path:'/index')]
     public function index()
     {
         $user = $this->request->input('user', 'Hyperf');
